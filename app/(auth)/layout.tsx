f07a15f687da08c0f116cfaf7a16311d9cf2c780
AlirezaTkQliro <alireza.tavakol@qliro.com>
2023-07-20 21:25:52 +0200

    base layout and architcture

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import '@/styles/global.css'
+import GlassPane from '@/components/GlassPane'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({
+  variable: '--greek',
+})
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function AuthRootLayout({ children }: Props) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  )
+}
